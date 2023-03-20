3a63dd69d7bebb8cdb43525b837ce3a60b627b13
Jon Goldberg <jon@megaphonetech.com>
2023-03-20 10:18:14 -0400

    support payment method; don't double-count credit payments; use 'Paid', not 'Amount'

--- payments.php
+++ payments.php
@@ -29,21 +29,22 @@ foreach ($records as $record) {
 fclose($iif);
 
 function writePaymentHeaders($iif) : void {
-  $headers = '!TRNS	TRNSTYPE	DATE	NAME	AMOUNT' . "\n";
-  $headers .= '!SPL	TRNSTYPE	DATE	NAME	AMOUNT' . "\n";
-  $headers .= '!ENDTRNS			' . "\n";
+  $headers = '!TRNS	TRNSTYPE	DATE	NAME	AMOUNT	PAYMETH' . "\n";
+  $headers .= '!SPL	TRNSTYPE	DATE	NAME	AMOUNT	' . "\n";
+  $headers .= '!ENDTRNS				' . "\n";
   fwrite($iif, $headers);
 }
 
 function writePayment($iif, array $record) : void {
-  // Seems like an invoice paid by credit and otherwise shows twice, once without an amount. Let's skip.
-  if (!$record['Amount']) {
+  // Don't apply credits twice.
+  if ($record['Method'] === 'Apply Credit') {
     return;
   }
   $date = (new DateTime($record['Payment Date']))->format('n/j/Y');
   $client = $record['Client'];
-  $amount = preg_replace('/[^0-9.]/s', '', $record['Amount']);
-  $trns = "TRNS	PAYMENT	$date	$client	$amount\n";
+  $amount = preg_replace('/[^0-9.]/s', '', $record['Paid']);
+  $paymentMethod = $record['Method'];
+  $trns = "TRNS	PAYMENT	$date	$client	$amount	$paymentMethod\n";
   $spl = "SPL	PAYMENT	$date		-$amount\n";
   $endtrns = "ENDTRNS			\n";
   fwrite($iif, $trns);
